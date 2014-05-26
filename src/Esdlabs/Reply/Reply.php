adab7912113b88434d72dd0480e5413cbc1f2ed7
Víctor Cruz <victorcruz@esd.com.do>
2014-05-26 18:30:01 -0400

    - Errors table migration - Facade implementation

---
+++ src/Esdlabs/Reply/Reply.php
@@ -0,0 +1,54 @@
+<?php namespace Esdlabs\Reply;
+
+use Esdlabs\Reply\Models\Error;
+
+class Reply {
+
+    /**
+     * Return error to response 
+     * 
+     * @author Victor Cruz <victorcruz@esd.com.do>
+     * @param string $error_code            Error code
+     * @param integer $http_response_code   HTTP response
+     * @param string $output_format         Response http format (JSON, XML, CSV)
+     *
+     * @return mixed
+     */
+    public function error($error_code, $notes = false, $http_response_code = false, $output_format = false)
+    {
+        $error = Error::where('error_code', '=', $error_code)->first();
+        if ($error)
+            $response = $this->buildResponseBody($error->error_code, $error->description);
+        else
+        { 
+            $response = $this->buildResponseBody('UNK-ERROR', "Error $error_code not found on the database");
+            $http_response_code = 500;
+        }
+                        
+        if (!$http_response_code)
+            $http_response_code = $error->response_code;
+
+        if ($notes)
+            $response['notes'] = $notes;
+
+        return \Response::make($response, $http_response_code);
+    }
+
+    /**
+     * Build body response to the request
+     * 
+     * @author Victor Cruz <victorcruz@esd.com.do>
+     * @param string $error_code            Error code
+     * @param string $description           Error descriptionHTTP response
+     *
+     * @return array
+     */
+    protected function buildResponseBody($error_code, $description)
+    {
+        return array(
+            'error_code' => $error_code,
+            'description' => $description
+        );
+    }
+
+}
---
+++ src/migrations/2014_05_26_153856_create_reply_errors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReplyErrorsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reply_errors', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('error_code');
+            $table->integer('response_code')->unique();
+            $table->string('description');
+            $table->dateTime('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reply_errors');
+    }
+
+}
